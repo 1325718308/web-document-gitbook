a6b853df06a93cdbda4b586327a1ace38554b0c9
wb-djy589366 <wb-djy589366@autonavi.com>
2020-11-25 18:07:45 +0800

    feat: vue3-api

---
+++ vueDocument/vue3-api/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  
+</template>
+
+<script>
+import { ref, watch, watchEffect } from 'vue'
+export default {
+  name: 'App',
+  setup() {
+    const data = ref(null);
+    watchEffect(async () => {
+      
+    })
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ vueDocument/vue3-api/src/components/Test.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>
+      {{attrs.title}}
+  </h1>
+</template>
+
+<script>
+import {toRefs} from 'vue'
+export default {
+    setup(props, context) {
+        return {
+           attrs:context.attrs
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
